8e238e53063cf7099861d0ea2c7a83ca98a22fcc
Puzanira <puzanova.ira2011@yandex.ru>
2018-01-23 22:20:34 +0300

    Изменен blacklist и функция пункутации

--- README.md
+++ README.md
@@ -4,10 +4,20 @@ Estimates your passoword, gives it a score from 0 to 10
 
 ## Blacklist checkout:
 
-In file "blacklist.json" (in this repo) there is a list of 
-30 most common passwords (according to [SplashData](https://www.teamsid.com/worst-passwords-2017-full-list/))
+In file **"blacklist.json"** (in this repo) there is a list of 
+[500 most common passwords](https://github.com/danielmiessler/SecLists/blob/master/Passwords/500-worst-passwords.txt)
 
 This program checks if your password is in that list. If so, it will return **"0"**. 
+
+## Scores
+
+* **+2** if your password contains _lowcase letters_
+* **+2** if your password contains _uppercase letters_
+* **+2** if your password contains _digits_
+* **+4** if your password contains _special symbols (like !%@)_
+* **+lengh scores** - your password length divided by 2 (max 10)
+
+You can get maximum **20** scores. Then program divides it by 2 and prints out.
 
 ## How to use:
 
--- password_strength.py
+++ password_strength.py
@@ -1,24 +1,27 @@
 import re
 import getpass
-import json
+import string
+import os
 
 
-def load_from_json(filepath):
+def load_blacklist(filepath):
     with open(filepath, 'r') as file_handler:
-        return json.load(file_handler)
+        read_data = file_handler.read()
+    blacklist = re.findall(r"[\w']+", read_data)
+    return blacklist
 
 
 def check_symbols(password):
     score = sum([int(bool(re.search(r'[0-9]', password)) * 2),
                  int(bool(re.search(r'[a-z]', password)) * 2),
                  int(bool(re.search(r'[A-Z]', password)) * 2),
-                 int(bool(re.search(r'[!@#$%&*()~{}]', password)) * 4)])
+                 ((any(char for char in password
+                       if char in string.punctuation)) * 4)])
     return score
 
 
-def check_blacklist(password):
-    black_list = load_from_json('blacklist.json')
-    for word in black_list:
+def check_blacklist(password, blacklist):
+    for word in blacklist:
         if password == word:
             return -10
     return 0
@@ -26,14 +29,15 @@ def check_blacklist(password):
 
 def check_length(password):
     length_scores = len(password) / 2.0
-    if length_scores > 10:
-        length_scores = 10
+    max_length_scores = 10
+    if length_scores > max_length_scores:
+        length_scores = max_length_scores
     return length_scores
 
 
-def get_password_strength(password):
+def get_password_strength(password, blacklist):
     total_scores = 0
-    total_scores += check_blacklist(password)
+    total_scores += check_blacklist(password, blacklist)
     total_scores += check_length(password)
     total_scores += check_symbols(password)
     if total_scores < 0:
@@ -42,5 +46,9 @@ def get_password_strength(password):
 
 
 if __name__ == '__main__':
-    password = getpass.getpass('Пароль: ')
-    print(get_password_strength(password))
+    if os.path.isfile('blacklist.txt'):
+        password = getpass.getpass('Пароль: ')
+        blacklist = load_blacklist('blacklist.txt')
+        print(get_password_strength(password, blacklist))
+    else:
+        print('Error: No such file in directory')
